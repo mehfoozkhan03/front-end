50f4030457d2ac91f07a0aa2a3ed5344fccd31ae
mehfoozkhan03 <mehfooznettech786@gmail.com>
2026-06-11 18:02:15 +0530

    cod eof the day

---
+++ React_start/Redux/performance/src/fibo.js
@@ -0,0 +1,15 @@
+const myMemo = (func) => {
+  const cache = {};
+  return (val) => {
+    return cache[val] || (cache[val] = func(val));
+  };
+};
+
+const fibo = myMemo((n) => {
+  if (n <= 1) return n;
+  return fibo(n - 1) + fibo(n - 2);
+});
+console.time();
+const ans = fibo(4500);
+console.log(`🚀 ~ ans:`, ans);
+console.timeEnd();
