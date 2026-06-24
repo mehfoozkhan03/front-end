3159ab6001bc5745f93d62bb38e26ec3bbe2d487
mehfoozkhan03 <mehfooznettech786@gmail.com>
2026-06-24 15:58:34 +0530

    coeof the day filter 50%

--- React_start/Redux/Music/src/Components/MusicFilter.jsx
+++ React_start/Redux/Music/src/Components/MusicFilter.jsx
@@ -1,30 +1,31 @@
-import { memo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 
 const MusicFilter = () => {
   const { musicRecord } = useSelector((store) => store.music);
   // console.log("filter", musicRecord);
 
-  const [key] = useState(() => {
-    return (
-      musicRecord &&
-      musicRecord
-        ?.map((el) => {
-          let value = Object.values(el.genre).join("");
-          return value;
-        })
-        .reduce((acc, curr) => {
-          acc[curr] = (acc[curr] || 0) + 1;
-          console.log("acc", acc);
-          return acc;
-        }, {})
-    );
-  });
+  const key = useMemo(() => {
+    if (!musicRecord) return {};
+
+    return musicRecord?.reduce((acc, curr) => {
+      let genre = Object.values(curr.genre).join("");
+      acc[genre] = (acc[genre] || 0) + 1;
+      return acc;
+    }, {});
+  }, [musicRecord]);
 
   console.log("key", key);
+
   return (
     <div>
       <h2>MusicFilter</h2>
+      {Object.keys(key).map((el) => (
+        <div key={el} style={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+          <input type="checkbox" />
+          <h3>{el}</h3>
+        </div>
+      ))}
     </div>
   );
 };
